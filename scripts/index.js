344de19d3c23caa82744bb55e14ae895d5e11501
Sanchez Pavlov <ingabuga@yandex.ru>
2022-06-24 13:58:24 +0700

    feature: добавить шаблон в разметку и массив в скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -26,10 +26,6 @@ function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                     // Так мы можем определить свою логику отправки.
                                     // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    // Выберите элементы, куда должны быть вставлены значения полей
-    // Вставьте новые значения с помощью textContent ()
     nameProfile.textContent = nameInput.value; 
     jobProfile.textContent = jobInput.value;
     // Закрываем попап после добавления новых данных
