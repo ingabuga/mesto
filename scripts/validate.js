de27fbf9bd46e49ef908270d81c693b8c661506b
Sanchez Pavlov <ingabuga@yandex.ru>
2022-07-14 17:47:57 +0700

    feat: fixing problem with submit button

--- scripts/validate.js
+++ scripts/validate.js
@@ -55,53 +55,6 @@ const hideInputError = (input) => {
 };
 */
 
-/*
-//валидация без кнопки
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.add('popup__text_input_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__text_error_active');
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove('popup__text_input_error');
-  errorElement.classList.remove('popup__text_error_active');
-  errorElement.textContent = '';
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__text'));
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__container'));
-  console.log(formList);
-  formList.forEach((formElement) => {
-  formElement.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    });
-    setEventListeners(formElement);
-  }); 
-};
-
-enableValidation();
-*/
-
 
 
 //валидация с деактивацией кнопки отправки
@@ -130,8 +83,10 @@ const checkInputValidity = (formElement, inputElement) => {
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__text'));
   const buttonElement = formElement.querySelector('.popup__save-button');
+  
+  console.log(buttonElement.disabled)
   toggleButtonState(inputList, buttonElement);
-
+  
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
       checkInputValidity(formElement, inputElement);
@@ -155,7 +110,6 @@ const enableValidation = () => {
 };
 
 
-
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
@@ -172,3 +126,4 @@ const toggleButtonState = (inputList, buttonElement) => {
 }; 
 
 enableValidation();
+
