2af12e0fa464cda1c7881120b9bd96cdf9d08fa8
Sanchez <ingabuga@yandex.ru>
2022-07-31 16:25:01 +0700

    feat: create class FormValidator

--- scripts/index.js
+++ scripts/index.js
@@ -145,7 +145,15 @@ class Card {
 */
 
 class FormValidator {
-
+  constructor(data, profilePopup) {
+    this._formSelector = data.formSelector;
+    this._inputSelector = data.inputSelector;
+    this._submitButtonSelector = data.submitButtonSelector;
+    this._inactiveButtonClass = data.inactiveButtonClass;
+    this._inputErrorClass = data.inputErrorClass;
+    this._errorClass = data.errorClass;
+    this._currentForm = profilePopup;
+} 
 }
  
 function renderCard(data) { 
