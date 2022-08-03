7ba4d2d64ac82743054ccdc4c65c2b193584d57e
Sanchez Pavlov <ingabuga@yandex.ru>
2022-08-03 15:08:12 +0700

    fix: fix Card class

--- scripts/card.js
+++ scripts/card.js
@@ -1,32 +1,3 @@
-/*
-const initialCards = [
-    {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
-*/
-
 class Card {
   constructor(data, templateSelector, handlePreview) {
       this._title = data.name;
@@ -44,20 +15,22 @@ class Card {
 
   generateCard() {
     this._element = this._getTemplate();
+    this._cardImage = this._element.querySelector('.elements__image');
+    this._likeButton = this._element.querySelector('.elements__like');
     this._setEventListeners();
-    this._element.querySelector('.elements__image').src = `${this._image}`;
+    this._cardImage.src = `${this._image}`;
     this._element.querySelector('.elements__title').textContent = this._title; 
-    this._element.querySelector('.elements__image').alt = this._title;
+    this._cardImage.alt = this._title;
     return this._element;
   }
 
 
   _setEventListeners() {
-    this._element.querySelector('.elements__image').addEventListener('click', () => {
+    this._cardImage.addEventListener('click', () => {
       this._handlePreview(this._image, this._title)// открываем попап
     });
-    
-    this._element.querySelector('.elements__like').addEventListener('click', () => {
+
+    this._likeButton.addEventListener('click', () => {
       this._handleLike()// лайкаем карточку
     });
 
@@ -67,11 +40,12 @@ class Card {
   }
 
   _handleLike() {
-    this._element.querySelector('.elements__like').classList.toggle('elements__like_active');
+    this._likeButton.classList.toggle('elements__like_active');
   }
 
   _handleDelete() {
-    this._element.remove(); 
+    this._element.remove();
+    this._element = null; 
   }
 }
 
