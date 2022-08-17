0997887e6b83427452e97423ddb33b46c5855908
Sanchez Pavlov <ingabuga@yandex.ru>
2022-08-17 17:32:56 +0700

    feat: working esc keydown

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -2,16 +2,17 @@ export default class Popup {
     constructor(popupSelector) {
         // this._popup = document.querySelector(popupSelector);
         this._popup = popupSelector;
+        // this._handleEscClose = this._handleEscClose.bind(this);
     }
 
     open () {
         this._popup.classList.add('popup_opened');
-        document.addEventListener('keydown', this._handlerEscClose); 
+        document.addEventListener('keydown', this._handlerEscClose.bind(this)); 
     }
 
     close () {
         this._popup.classList.remove('popup_opened');
-        document.removeEventListener('keydown', this._handlerEscClose);
+        document.removeEventListener('keydown', this._handlerEscClose.bind(this));
     }
 
     _handlerEscClose(evt) {
