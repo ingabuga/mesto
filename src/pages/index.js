6506fb83a7d312ce2e702dc48906dc4b5850a7be
Sanchez <ingabuga@yandex.ru>
2022-09-07 11:25:12 +0700

    fix: remove import class myCard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 
 import Card from '../components/Card.js';
-import MyCard from '../components/MyCard.js';
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
 import PopupWithImage from '../components/PopupWithImage.js';
